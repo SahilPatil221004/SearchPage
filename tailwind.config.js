a58a2ec72cefec4a438d02868043cff5bf0d0cfa
Sahil Patil <135319155+SahilPatil221004@users.noreply.github.com>
2024-03-11 14:39:15 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors:{
+        blue:'#61A3BA',
+        fblue:'#A2C579',
+        black:'black'
+      }
+    },
+  },
+  plugins: [],
+}
+
